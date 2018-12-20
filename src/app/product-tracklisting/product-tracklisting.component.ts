76620581f1599de67262f13dece55d104c74a9fa
Evan McStay <evan.mcstay@fedex.com>
2018-12-20 09:58:51 -0500

    Adding in the product-list component and setting it up to receive data from the product JSON file. Modifying the landing page to show the products

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,10 +9,15 @@ import { Album } from '../album';
 })
 export class ProductTracklistingComponent implements OnInit {
 
+  // Stores the value of the chained subscribe method.
+  // The Album interface is used here to define the data that albumInfo can store.
   albumInfo: Album;
 
+  // Good practice to inject an instance of a service (i.e ProductService) into the constructor of a class.
   constructor(private _productService: ProductService) { }
 
+  // Call the product service's getAlbum() method and pass in 1.
+  // Then chain the subscribe method to get the response (in JSON) which is stored inside the albumInfo class variable.
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+
+  private _productsUrl = '../assets/products.json';
 
   // Good practice to inject an instance of a service (i.e Http) into the constructor of a class.
   constructor(private _http: Http) { }
@@ -16,5 +19,9 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     // Issues an HTTP call which is then chained and mapped as JSON which is casted as type Album.
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
