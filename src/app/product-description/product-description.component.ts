01380d97ed99607ae341a52136b2a64c54bdc11b
Evan McStay <evan.mcstay@fedex.com>
2018-12-20 08:22:56 -0500

    Adding in the product service and updating the components to get and display the data in the template.

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  // Stores the value of the chained subscribe method
+  albumInfo;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    // Call the product service's getAlbum() method and pass in 1.
+    // Then chain the subscribe method to get the response (in JSON) which is stored inside the albumInfo class variable.
+    this._productService.getAlbum(1)
+      .subscribe(response => this.albumInfo = response);
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  // Good practice to inject an instance of a service (i.e Http and Response) into the constructor of a class.
+  constructor(private _http: Http) { }
+
+  // Return the result of a HTTP GET request mapped as JSON.
+  getAlbum(id: number) {
+    // Issues an HTTP call which is then chained and mapped as JSON.
+    return this._http.get(this._albumUrl)
+      .map((response) => response.json());
+
+  }
+}
